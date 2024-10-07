9d3a9ae25c3444788777151e497f0e895dc1e5dc
Keesha Kallasur <keeshakallasur@gmail.com>
2024-10-07 15:33:43 +0200

    Update and made changes

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
 function generatePsw() {
     let output = document.getElementById("output");
-    let chars =
-      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@1#_-*&%+=/";
-    let length = document.getElementById("length").value - 1; //-1 for index to be accurate
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#_-*&%+=/";
+    let length = parseInt(document.getElementById("length").value); 
     let password = "";
-    for (let i = 0; i <= length; i++) {
-      let random = Math.floor(Math.random() * chars.length);
-      password += chars.substring(random, random + 1);
+
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
     }
+
     output.value = password;
-  }
-  // copy password to clipboard
-  function copyPsw() {
+}
+
+function copyPsw() {
+    let output = document.getElementById("output");
     output.select();
+    output.setSelectionRange(0, 99999); // For mobile compatibility
     document.execCommand("copy");
-  }
+    alert("Password copied to clipboard!");
+}
